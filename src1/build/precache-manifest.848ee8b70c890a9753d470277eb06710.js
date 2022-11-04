82fd961722866cf0e25a4cd6dbad99d3838d890f
pradeep85841 <97498198+pradeep85841@users.noreply.github.com>
2022-11-04 19:10:26 +0530

    Add files via upload

---
+++ src1/build/precache-manifest.848ee8b70c890a9753d470277eb06710.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6cc0de87c64ef27542e0",
+    "url": "/static/css/main.efd96dec.chunk.css"
+  },
+  {
+    "revision": "6cc0de87c64ef27542e0",
+    "url": "/static/js/main.6cc0de87.chunk.js"
+  },
+  {
+    "revision": "35640e69b5201224c966",
+    "url": "/static/js/1.35640e69.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5146ac5a1e7be28ade1e553d1601c311",
+    "url": "/static/media/SCET_0005.5146ac5a.png"
+  },
+  {
+    "revision": "9003ff9d4b89b7db3e5f8d243bf08f7e",
+    "url": "/index.html"
+  }
+];
